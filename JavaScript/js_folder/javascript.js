bfd4e3471721a8df37faefc16dbb80e8fc0bc67a
Junseong <bws4115@naver.com>
2024-12-17 09:40:56 +0900

    파일 추가가

---
+++ JavaScript/js_folder/javascript.js
@@ -0,0 +1,9 @@
+/*
+    Documents.write()는 자바스크립트에서 HTML 문서에 텍스트를
+    직접 작성할 때 사용한다
+    HTML을 처리하는 방식으로 문자열을 출력.
+*/
+
+document.write("hello world <br> www111111");
+document.write("hello world <br> www111111");
+document.write("hello world <br> www111111");
